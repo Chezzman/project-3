d3f40eb7511adc4bc96b7769f8a113159971d661
Charles George van Oeffelen <charlesvo@spartaglobal.co>
2017-04-04 11:29:56 +0100

    Adding in heroku

---
+++ api/config/router.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+//var trumpController = require('../controllers/trump');
+
+// router.route('/tump')
+//   .get(trumpController.getAll)
+//   .post(trumpController.createDuck);
+//
+// router.route('/trump/:id')
+//   .get(trumpController.getDuck)
+//   .patch(trumpController.updateDuck)
+//   .delete(trumpController.removeDuck);
+
+module.exports = router;
