353294ba772bc2cfe4bed885604c1cd2d640fbb1
Charles George van Oeffelen <charlesvo@spartaglobal.co>
2017-04-10 17:20:54 +0100

    adding a popover for the search nav

--- public/js/factories/tweet.factory.js
+++ public/js/factories/tweet.factory.js
@@ -9,41 +9,11 @@ function TweetFactory($http){
         }
       });
     }
-    // searchTwitter: function () {
-    //   return $http({
-    //     method: 'GET',
-    //     url: `/api/tweets/search`
-    //   });
-    // }
-    // getOne: function (tweetId){
-    //   return $http({
-    //     method: 'GET',
-    //     url: `${API_URL}/tweets/${tweetId}`
-    //   });
-    // },
-    // createOne: function (newTweet) {
-    //   return $http({
-    //     method: 'POST',
-    //     url: `${API_URL}/tweets`,
-    //     data: newTweet
-    //   });
-    // },
-    // deleteOne: function (tweetId) {
-    //   return $http({
-    //     method: 'DELETE',
-    //     url: `${API_URL}/tweets/${tweetId}`
-    //   });
-    // },
-    // updateOne: function (editedTweet) {
-    //   return $http({
-    //     method: 'PATCH',
-    //     url: `${API_URL}/tweets/${editedTweet._id}`,
-    //     data: editedTweet
-    //   });
+
   };
 }
 
-//factory makes the reguest to server side
+
 
 
 
