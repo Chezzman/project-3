c8980ac3834ffe21f4c5c77c28cd244d256fca39
Charles George van Oeffelen <charlesvo@spartaglobal.co>
2017-04-11 11:33:04 +0100

    Adding markers on google maps for tweets

--- public/js/controllers/tweet.controller.js
+++ public/js/controllers/tweet.controller.js
@@ -5,7 +5,17 @@ function TweetController(TweetFactory) {
     TweetFactory.searchTweets(controller.tweetSearch).then(
       function success(response) {
         controller.allTweets = response.data;
+        controller.markers = controller.allTweets.statuses.map(function(tweet, i) {
+          return {
+            location: {
+              latitude: tweet.place.bounding_box.coordinates[0][0][1],
+              longitude: tweet.place.bounding_box.coordinates[0][0][0]
+            },
+            id: i
+          };
+        });
         console.log('twitter response: ', response.data);
+        console.log(controller.markers);
       },
       function error(error) {
         console.warn('Error getting tweets:', error);
@@ -19,6 +29,7 @@ function TweetController(TweetFactory) {
 
   function init(){
     controller.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    controller.markers = [];
     controller.tweetSearch = '';
     controller.allTweets = [];
   }
